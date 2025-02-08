0f5dcf03545bd7522f1f5e97192cdc71b71b1143
pabitra bera <madeeasycodinglife@gmail.com>
2025-02-08 23:16:53 +0530

    added swagger to expense-service.

--- expense-service/src/main/java/com/madeeasy/ExpenseServiceApplication.java
+++ expense-service/src/main/java/com/madeeasy/ExpenseServiceApplication.java
@@ -1,5 +1,12 @@
 package com.madeeasy;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -9,6 +16,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Expense Management Service API",
+                version = "1.0",
+                description = "API for managing and tracking company expenses, generating invoices, and analyzing expense trends by category and time period.",
+                contact = @Contact(
+                        email = "madeeasycodinglife@gmail.com",
+                        name = "Pabitra Bera",
+                        url = "https://www.linkedin.com/in/pabitra-bera"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://demo.org/licenses/LICENSE-2.0"
+                ),
+                termsOfService = "Terms and Conditions for accessing the Expense Management API.",
+                summary = "This API provides endpoints for submitting, retrieving, updating, deleting expenses, generating invoices, and analyzing monthly/yearly expense trends."
+        )
+)
+
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        description = "Bearer with JWT authentication"
+)
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
