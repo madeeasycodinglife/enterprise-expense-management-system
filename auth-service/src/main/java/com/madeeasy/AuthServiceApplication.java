e66689a658c0b0b6ff37f88208695017b25fa3ad
pabitra bera <madeeasycodinglife@gmail.com>
2025-02-08 23:16:35 +0530

    added swagger to auth-service.

--- auth-service/src/main/java/com/madeeasy/AuthServiceApplication.java
+++ auth-service/src/main/java/com/madeeasy/AuthServiceApplication.java
@@ -1,5 +1,12 @@
 package com.madeeasy;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -9,6 +16,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.client.RestTemplate;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Authentication Service API",
+                version = "1.0",
+                description = "API for managing user authentication, registration, session management, and user details retrieval.",
+                contact = @Contact(
+                        email = "madeeasycodinglife@gmail.com",
+                        name = "Pabitra Bera",
+                        url = "https://www.linkedin.com/in/pabitra-bera"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://demo.org/licenses/LICENSE-2.0"
+                ),
+                termsOfService = "Terms and Conditions for accessing the Authentication API.",
+                summary = "This API provides endpoints for user registration (sign-up), login (sign-in), token management (refresh and validation), and user profile management (update and retrieval)."
+        )
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        description = "Bearer with JWT authentication"
+)
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
