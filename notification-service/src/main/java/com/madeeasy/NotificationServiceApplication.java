c46ad2e6dfcaa1b3ff87a1f7279b1f5cdb3820bf
pabitra bera <madeeasycodinglife@gmail.com>
2025-02-08 23:17:21 +0530

    added swagger to notification-service.

--- notification-service/src/main/java/com/madeeasy/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/madeeasy/NotificationServiceApplication.java
@@ -1,5 +1,12 @@
 package com.madeeasy;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -8,6 +15,33 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Notification Service API",
+                version = "1.0",
+                description = "API for handling notification requests related to expense approval, including sending approval/rejection notifications.",
+                contact = @Contact(
+                        email = "madeeasycodinglife@gmail.com",
+                        name = "Pabitra Bera",
+                        url = "https://www.linkedin.com/in/pabitra-bera"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://demo.org/licenses/LICENSE-2.0"
+                ),
+                termsOfService = "Terms and Conditions for accessing the Notification API.",
+                summary = "This API provides an endpoint for sending notifications related to expense approval or rejection, including parsing and processing expense details."
+        )
+)
+
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        description = "Bearer with JWT authentication"
+)
 @SpringBootApplication
 @EnableDiscoveryClient
 public class NotificationServiceApplication {
