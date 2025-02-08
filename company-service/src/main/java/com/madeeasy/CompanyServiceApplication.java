a7c28c2e1a15b8324c0220a9f5c997f8b2fa07b0
pabitra bera <madeeasycodinglife@gmail.com>
2025-02-08 23:16:13 +0530

    added swagger to company-service.

--- company-service/src/main/java/com/madeeasy/CompanyServiceApplication.java
+++ company-service/src/main/java/com/madeeasy/CompanyServiceApplication.java
@@ -1,9 +1,31 @@
 package com.madeeasy;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Company Service API",
+                version = "1.0",
+                description = "API for managing company registrations and retrieving company information by domain name.",
+                contact = @Contact(
+                        email = "madeeasycodinglife@gmail.com",
+                        name = "Pabitra Bera",
+                        url = "https://www.linkedin.com/in/pabitra-bera"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://demo.org/licenses/LICENSE-2.0"
+                ),
+                termsOfService = "Terms and Conditions for accessing the Company API.",
+                summary = "This API provides endpoints for registering companies and retrieving company details by their domain name."
+        )
+)
 @SpringBootApplication
 @EnableDiscoveryClient
 public class CompanyServiceApplication {
