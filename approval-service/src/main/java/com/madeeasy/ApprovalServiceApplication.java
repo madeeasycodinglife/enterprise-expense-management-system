e278b6d33cbe8ef5c5030fff57fd0fe14c281285
pabitra bera <madeeasycodinglife@gmail.com>
2025-02-08 23:15:58 +0530

    added swagger to approval-service.

--- approval-service/src/main/java/com/madeeasy/ApprovalServiceApplication.java
+++ approval-service/src/main/java/com/madeeasy/ApprovalServiceApplication.java
@@ -1,5 +1,12 @@
 package com.madeeasy;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -8,6 +15,32 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Approval Service API",
+                version = "1.0",
+                description = "API for handling approval requests for expenses",
+                contact = @Contact(
+                        email = "madeeasycodinglife@gmail.com",
+                        name = "Pabitra Bera",
+                        url = "https://www.linkedin.com/in/pabitra-bera"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://demo.org/licenses/LICENSE-2.0"
+                ),
+                termsOfService = "Terms and Conditions for accessing the Approval API.",
+                summary = "This API provides endpoints for submitting expense approval requests."
+        )
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        description = "Bearer with JWT authentication"
+)
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApprovalServiceApplication {
@@ -20,6 +53,7 @@ public class ApprovalServiceApplication {
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
+
     // Non-load-balanced RestTemplate for specific host calls (e.g., localhost)
     @Bean
     public RestTemplate localRestTemplate(RestTemplateBuilder builder) {
